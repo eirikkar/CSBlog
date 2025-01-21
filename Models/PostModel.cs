4a57277ac12454a56c576d39939fd0026cab2bf6
eirikkar <eirikkar@proton.me>
2025-01-21 09:23:41 +0100

    add Postmodel

---
+++ Models/PostModel.cs
@@ -0,0 +1,10 @@
+namespace CSBlog.Models;
+
+public class PostModel
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
