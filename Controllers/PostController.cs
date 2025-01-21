5b6d50ee0ba6bf34f3257662d2b51e1e306aebd2
eirikkar <eirikkar@proton.me>
2025-01-21 09:52:47 +0100

    add constructor, readonly field context and get method

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,10 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using CSBlog.Data;
+using CSBlog.Models;
 namespace CSBlog.Controllers;
 
 [ApiController]
 [Route("[api/posts]")]
 public class PostController : ControllerBase
 {
+    private readonly AppDbContext _context;
 
+    public PostController(AppDbContext context)
+    {
+        _context = context;
+    }
 
+    [HttpGet]
+    public IEnumerable<PostModel> Get()
+    {
+        return _context.Posts.ToList();
+    }
 }
