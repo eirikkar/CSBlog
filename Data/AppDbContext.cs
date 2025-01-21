7f5c7ed1d5d46648802e5dcdaa77fb4b4c6542f1
eirikkar <eirikkar@proton.me>
2025-01-21 09:38:47 +0100

    override on modelcreating method and adding posts to migration

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -10,5 +10,19 @@ public class AppDbContext : DbContext
 
     public required DbSet<PostModel> Posts { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<PostModel>(entity =>
+        {
+            entity.ToTable("Posts");
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Title).IsRequired();
+            entity.Property(e => e.Content).IsRequired();
+            entity.Property(e => e.CreatedAt).IsRequired();
+            entity.Property(e => e.UpdatedAt).IsRequired();
+        });
+    }
 
 }
