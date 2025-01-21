76a56fdc7125f7b17b190f20564094d912decfe2
eirikkar <eirikkar@proton.me>
2025-01-21 12:18:46 +0100

    add UserModel

---
+++ Models/UserModel.cs
@@ -0,0 +1,9 @@
+namespace CSBlog.Models;
+
+public class UserModel
+{
+    public Guid Id { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public string? Email { get; set; }
+}
