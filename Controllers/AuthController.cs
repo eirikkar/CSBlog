f5f25004bd953ae91922cc02e5b580252c95bd4a
eirikkar <eirikkar@proton.me>
2025-01-23 12:54:05 +0100

    added nullcheck to authcontroller

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -5,6 +5,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using CSBlog.Models;
 using CSBlog.Data;
+using Microsoft.Extensions.Configuration;
 
 namespace CSBlog.Controllers
 {
@@ -27,6 +28,11 @@ namespace CSBlog.Controllers
             var verifyUser = _context.Users.SingleOrDefault(u => u.Username == user.Username);
             if (verifyUser != null && BCrypt.Net.BCrypt.Verify(user.Password, verifyUser.Password))
             {
+                if (string.IsNullOrEmpty(user.Username))
+                {
+                    return BadRequest("Username is required.");
+                }
+
                 var token = GenerateJwtToken(user.Username);
                 return Ok(new { token });
             }
@@ -35,13 +41,24 @@ namespace CSBlog.Controllers
 
         private string GenerateJwtToken(string username)
         {
+            if (string.IsNullOrEmpty(username))
+            {
+                throw new ArgumentNullException(nameof(username), "Username is required.");
+            }
+
             var claims = new[]
             {
                 new Claim(JwtRegisteredClaimNames.Sub, username),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
             };
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var keyString = _configuration["Jwt:Key"];
+            if (string.IsNullOrEmpty(keyString))
+            {
+                throw new ArgumentNullException("Jwt:Key", "JWT key is not configured.");
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(keyString));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
             var token = new JwtSecurityToken(
@@ -55,3 +72,4 @@ namespace CSBlog.Controllers
         }
     }
 }
+
