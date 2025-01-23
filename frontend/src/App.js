a1db688e88827148ab9902601c6b23f3df560b3b
eirikkar <eirikkar@proton.me>
2025-01-23 14:25:55 +0100

    update app.js

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import Home from "./Home";
+function App() {
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/home" element={<Home />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
