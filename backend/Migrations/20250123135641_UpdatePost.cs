6fdaeda96b6a487c6f598364801fb94951f46df6
eirikkar <eirikkar@proton.me>
2025-01-23 15:01:28 +0100

    updated migrations

---
+++ backend/Migrations/20250123135641_UpdatePost.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CSBlog.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Users_Id",
+                table: "Posts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Users_Id",
+                table: "Posts",
+                column: "Id",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
