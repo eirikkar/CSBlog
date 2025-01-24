2cf0a56f9a7dc1a6672998952065ca5148f744af
eirikkar <eirikkar@proton.me>
2025-01-24 09:46:51 +0100

    updated migrations

--- backend/Migrations/20250124083930_AddUniqueConstrainsToUsername.Designer.cs
+++ backend/Migrations/20250124083930_AddUniqueConstrainsToUsername.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CSBlog.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CSBlog.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250124083930_AddUniqueConstrainsToUsername")]
+    partial class AddUniqueConstrainsToUsername
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.1");
