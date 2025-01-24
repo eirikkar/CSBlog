927acf69985e34e6aa9d016eeab6712fb6b2226b
eirikkar <eirikkar@proton.me>
2025-01-24 12:46:09 +0100

    rebuild react project

--- frontend/src/Post.jsx
+++ frontend/src/Post.jsx
@@ -3,27 +3,27 @@ import { useParams } from "react-router-dom";
 import { getPost } from "./api";
 
 const Post = () => {
-  const { id } = useParams();
-  const [post, setPost] = useState(null);
+    const { id } = useParams();
+    const [post, setPost] = useState(null);
 
-  useEffect(() => {
-    async function fetchPost() {
-      const data = await getPost(id);
-      setPost(data);
+    useEffect(() => {
+        async function fetchPost() {
+            const data = await getPost(id);
+            setPost(data);
+        }
+        fetchPost();
+    }, [id]);
+
+    if (!post) {
+        return <div>Loading...</div>;
     }
-    fetchPost();
-  }, [id]);
 
-  if (!post) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.content}</p>
-    </div>
-  );
+    return (
+        <div>
+            <h1>{post.title}</h1>
+            <p>{post.content}</p>
+        </div>
+    );
 };
 
 export default Post;
