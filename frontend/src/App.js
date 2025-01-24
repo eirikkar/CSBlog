bc8b499e59ad0fc416189c36f350d9a90fcb82b4
eirikkar <eirikkar@proton.me>
2025-01-24 09:08:09 +0100

    add home to root and login to /login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import Home from "./Home";
-import Post from "./Post"; // Import the new Post component
+import Post from "./Post";
 
 function App() {
     return (
         <Router>
             <div className="App">
                 <Routes>
-                    <Route path="/" element={<Login />} />
-                    <Route path="/home" element={<Home />} />
+                    <Route path="/" element={<Home />} />
                     <Route path="/post/:id" element={<Post />} />
+                    <Route path="/login" element={<Login />} />
                 </Routes>
             </div>
         </Router>
