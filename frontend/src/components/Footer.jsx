384f9d331dfae4f6942881e2f939898ac8d8a7b5
eirikkar <eirikkar@proton.me>
2025-01-26 14:48:43 +0100

    removed social media from footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,16 +6,16 @@ const Footer = () => {
       <div className="container text-center">
         <div className="row">
           {/* About Section */}
-          <div className="col-md-4 mb-3 mb-md-0">
+          <div className="col-md-6 mb-3 mb-md-0">
             <h5>About</h5>
             <p>
-              MyModernBlog is a platform to share insightful articles and
-              engaging content.
+              Dette er min personlige blogg, hvor jeg skriver om ting som
+              interesserer meg. Jeg håper du finner noe interessant her!
             </p>
           </div>
 
           {/* Quick Links */}
-          <div className="col-md-4 mb-3 mb-md-0">
+          <div className="col-md-6 mb-3 mb-md-0">
             <h5>Quick Links</h5>
             <ul className="list-unstyled">
               <li>
@@ -35,24 +35,10 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-
-          {/* Social Media */}
-          <div className="col-md-4">
-            <h5>Follow Us</h5>
-            <a href="#" className="me-2 text-white">
-              <i className="bi bi-facebook" style={{ fontSize: "1.5rem" }}></i>
-            </a>
-            <a href="#" className="me-2 text-white">
-              <i className="bi bi-twitter" style={{ fontSize: "1.5rem" }}></i>
-            </a>
-            <a href="#" className="text-white">
-              <i className="bi bi-instagram" style={{ fontSize: "1.5rem" }}></i>
-            </a>
-          </div>
         </div>
         <hr className="my-3" />
         <p className="mb-0">
-          &copy; {new Date().getFullYear()} MyModernBlog. All rights reserved.
+          &copy; {new Date().getFullYear()} Eiriks Blogg. All rights reserved.
         </p>
       </div>
     </footer>
