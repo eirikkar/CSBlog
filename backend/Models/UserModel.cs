fb2cbf90a4835fbc5dd0ad98f5399d25dc4699e2
eirikkar <eirikkar@proton.me>
2025-01-28 09:59:28 +0100

    Add roles to UserModel

--- backend/Models/UserModel.cs
+++ backend/Models/UserModel.cs
@@ -1,9 +1,16 @@
 namespace CSBlog.Models;
 
+public enum UserRole
+{
+    Admin,
+    User
+}
+
 public class UserModel
 {
     public Guid Id { get; set; }
     public string? Username { get; set; }
     public string? Password { get; set; }
     public string? Email { get; set; }
+    public UserRole Role { get; set; } = UserRole.User;
 }
