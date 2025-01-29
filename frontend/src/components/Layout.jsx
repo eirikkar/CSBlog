bba63f8dcccafab55e7369081419b9fafe81ee84
eirikkar <eirikkar@proton.me>
2025-01-29 14:02:35 +0100

    add propTypes to to Layout

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,16 +1,18 @@
-// src/components/Layout.jsx
-import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import PropTypes from "prop-types";
 
 const Layout = ({ children }) => {
-  return (
-    <div className="d-flex flex-column min-vh-100">
-      <Navbar />
-      <main className="flex-fill container mt-4">{children}</main>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="d-flex flex-column min-vh-100">
+            <Navbar />
+            <main className="flex-fill container mt-4">{children}</main>
+            <Footer />
+        </div>
+    );
 };
 
+Layout.propTypes = {
+    children: PropTypes.node.isRequired,
+};
 export default Layout;
