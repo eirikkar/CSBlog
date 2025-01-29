a57e6a2d2b8c88054718d12981878e38c99b73c5
eirikkar <eirikkar@proton.me>
2025-01-29 13:59:08 +0100

    removed React from import

--- frontend/src/components/Admin.jsx
+++ frontend/src/components/Admin.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { getPosts, deletePost, createPost, deleteImage } from "../api";
