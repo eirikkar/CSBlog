65494076e9ae754101101b1954921442c5ae18c9
eirikkar <eirikkar@proton.me>
2025-01-30 10:19:56 +0100

    add route to editprofile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,23 +5,26 @@ import Post from "./components/Post";
 import Admin from "./components/Admin";
 import Layout from "./components/Layout";
 import SearchResults from "./components/SearchResults";
+import EditProfile from "./components/EditProfile";
 
 function App() {
-    return (
-        <Router>
-            <Layout>
-                <div className="container mt-4">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/search" element={<SearchResults />} />
-                        <Route path="/post/:id" element={<Post />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/admin" element={<Admin />} />
-                    </Routes>
-                </div>
-            </Layout>
-        </Router>
-    );
+  return (
+    <Router>
+      <Layout>
+        <div className="container mt-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<SearchResults />} />
+            <Route path="/post/:id" element={<Post />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/admin/edit-profile" element={<EditProfile />} />
+            <Route path="*" element={<h1>Not Found</h1>} />
+          </Routes>
+        </div>
+      </Layout>
+    </Router>
+  );
 }
 
 export default App;
